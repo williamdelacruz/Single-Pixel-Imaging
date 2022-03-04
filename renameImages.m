492d0a6107347231db0376cb3b29409cab08df64
William De la Cruz <33740394+williamdelacruz@users.noreply.github.com>
2022-03-03 19:45:12 -0600

    Add files via upload

---
+++ renameImages.m
@@ -0,0 +1,14 @@
+% Re-nombra archivos
+db_path = '\coco-train2014\';
+Files = dir('.\coco-train2014\*.jpg');
+
+
+for k=1:length(Files)
+   FileNames = Files(k).name;
+   old_name = strcat(path_dir,db_path,FileNames);
+   name = strcat('COCO_train2014_', num2str(k), '.jpg');
+   new_name = strcat(path_dir,db_path,name);
+   movefile(old_name, new_name)
+end
+
+
